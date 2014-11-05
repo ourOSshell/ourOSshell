cb4893ac033feca083f9d6d2086a709a9bb6cf52
TommyymmoT <fawcetttom@hotmail.com>
2014-11-05 21:27:08 +0000

    Added some commands and the start to our process handler.

--- ourOSshell.c
+++ ourOSshell.c
@@ -2,11 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 
+int processHandler()
+{
+    printf("Dealing with Processes (pid:%d)\n", (int) getpid());
+    int rc = fork() ; 
+
+    if (rc < 0) 
+    {
+        //error forking
+        exit(1);
+    } 
+
+    else if (rc == 0) 
+    {
+        //child code
+
+    } 
+
+    else
+    {
+        //Parent process code
+        int wc = wait(NULL); //is child finished?
+    }
+}
+
 int main(){
+
+    //opening ascii art
+    printf("  ,---. ,--.          ,--,--. \n");
+    printf(" '   .-'|  ,---. ,---.|  |  | \n");
+    printf("  .  `-.|  .-.  | .-. |  |  | \n");
+    printf(" .-'    |  | |  |   --|  |  | \n");
+    printf(" `-----'`--' `--'`----`--`--' \n");
+    printf("\n");
+    printf("\n");
+
     char command[100];
     while(strcmp(command,"exit")!=0){
         printf("prompt-> ");
         scanf("%s", command);
-    }
+
+        // Some commands
+        if(strcmp(command,"vim")==0)
+        {
+            //enter text editor
+            system("vim");
+        }
+
+        else if(strcmp(command,"help")==0)
+        {
+            //These are the things you can do in ourOSshell
+            printf("So far you can't do anything. GoodLuck!\n");
+        }
+
+        //doesn't match any commands
+        else if(strcmp(command,"")==0)
+        {
+            printf("This is not a command. Check 'help' for commands.\n");
+        }
+
+        //valid command so use processHandler
+        else
+        {
+            //processHandler(command);
+        }	
+
+    }//end while 
     return 0;
 }
