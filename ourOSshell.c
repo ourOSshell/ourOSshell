56b0a6c61f09f8450f747ae92b3636d8feffe994
Matthew Musoke <msk.matt2@gmail.com>
2014-11-19 18:07:35 -0400

    Revert "Added the fork() which will eventually call exec()."
    This reverts commit 3b3ee87428e6dbe1a1ccac38400af9b471f30b97.

--- ourOSshell.c
+++ ourOSshell.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
 
 int main(){
     char command[100];
-    int rc;
-    while(1){
+    while(strcmp(command,"exit")!=0){
         printf("prompt-> ");
         scanf("%s", command);
-        if(strcmp(command,"exit")==0) exit(0);
-        rc = fork();
-        if(rc<0){
-            fprintf(stdout, "Could not create another process.\n\n");
-            exit(0);
-        }
-        else if(rc==0){
-            //Call exec() to run command
-            //Should not print if exec is successful
-            printf("Could not find command: %s\n\n", command);
-            exit(0);
-        }
-        else if(rc>0){
-            int wc = wait(NULL);
-	}
     }
     return 0;
 }
