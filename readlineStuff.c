80c0b931d03cfd15837c3ebc6406dd0fff9eedfc
Matthew Musoke <msk.matt2@gmail.com>
2014-11-20 23:41:37 -0400

    Added some documentation

--- readlineStuff.c
+++ readlineStuff.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Adds a character to the end of a string
 void append(char* str, char ch){
     int length = strlen(str);
     str[length] = ch;
@@ -9,11 +10,17 @@ void append(char* str, char ch){
 }
 
 int main(){
+    //whole command which must latter be parsed.
+    //Should be an array of strings
     char command[100] = "\0";
+    //last character read in
     char ch;
+    //will hold parsed string
     char *args[10];
+    //temporary values to test exec() call
     args[0] = strdup("ls");
     args[1] = NULL;
+    //return code for fork()
     int rc;
     //do{int c = getchar(); printf("c=%d\n", c);}while(1);
     while(1){
@@ -54,15 +61,22 @@ int main(){
                 append(command, ch);
             }
         }
+        //Return to normal capturing of keystrokes
         system("/bin/stty cooked echo");
         //to make sure we have the right string in the end
         printf("%s\n", command);
+        
+        //parse(command);
+        
+        //Exit loop if command is exit
+        //Should eventually be args[0]
         if(strcmp(command,"exit")==0) exit(0);
         rc = fork();
         if(rc<0){
             fprintf(stderr, "Could not fork\n\n");
             exit(0);
         }
+        //Child proccess where command is executed.
         else if(rc==0){
             //call exec() to run command
             execvp(args[0],args);
@@ -70,6 +84,7 @@ int main(){
             fprintf(stderr, "Could not find command: %s\n\n", command);
             exit(0);
         }
+        //Parent process
         else if(rc>0){
             wait(NULL);
         }
